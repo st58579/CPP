329d66801fdae3e5361b443c9e99335d795f875a
st58579 <72264702+st58579@users.noreply.github.com>
2020-12-25 14:57:12 +0100

    backup

---
+++ semestralka/JsonLibrary/JsonLibrary/api.cpp
@@ -0,0 +1,246 @@
+﻿#include "pch.h"
+#include "api.h"
+
+
+
+KeyValuePair::KeyValuePair(std::string key, Value* value) : key(key), value(value)
+{
+}
+
+std::string KeyValuePair::getKey() const
+{
+	return key;
+}
+
+Value* KeyValuePair::getValue() const
+{
+	return value;
+}
+
+BoolValue::BoolValue(bool value) : value(value)
+{
+}
+
+bool BoolValue::get() const
+{
+	return value;
+}
+
+NumberValue::NumberValue(double value) : value(value)
+{
+}
+
+double NumberValue::get() const
+{
+	return value;
+}
+
+StringValue::StringValue(std::string value) : value(value)
+{
+}
+
+std::string StringValue::get() const
+{
+	return value;
+}
+
+void StringValue::append(char c)
+{
+	value += c;
+}
+
+ArrayValue::ArrayValue()
+{
+	array = DynamicArray<Value*>();
+}
+
+ArrayValue::~ArrayValue()
+{
+	array.~DynamicArray();
+}
+
+void ArrayValue::append(Value* element)
+{
+	array.append(element);
+}
+
+ObjectValue::ObjectValue()
+{
+	array = DynamicArray<KeyValuePair>();
+}
+
+ObjectValue::~ObjectValue()
+{
+	array.~DynamicArray();
+}
+
+void ObjectValue::append(const KeyValuePair& pair)
+{
+	array.append(pair);
+}
+
+std::string ObjectValue::serialize() const
+{
+	return std::string();
+}
+
+// - provede deserializaci řetězce na vytvořené objekty
+// - přečtu znak a rozhodnu se (uvedený postup předpokládá čtení bez přeskakování bílých znaků)
+// -- '{' - začínám číst objekt
+// -------- čtu znaky, pak musí být dvojtečka, potom volám rekurzivně deserialize();  následuje čárka nebo '}', podle situace se čtení opakuje
+// -- '[' - začínám číst pole
+// -------- volám rekurzivně deserialize(); následuje čárka nebo ']', podle situace se čtení opakuje
+// -- '"' - začínám číst řetězec - pozor na escapované uvozovky
+// -- [-0123456789] - začínám číst číslo - načtu všechny číslice (pozor na možnou desetinnou  tečku)
+// -- 'n' - 'null' – načtu zbylé znaky a kontroluji, že je to opravdu ‚null‘ a ne něco jiného
+// -- 't' - 'true' - dtto
+// -- 'f' - 'false' - dtto
+// -- cokoliv jiného - vyvolávám výjimku
+// - není přípustné vracet nullptr
+// - deserializace musí být rozumně implementována - není přípustné zde napsat jednu extrémně  dlouhou metodu
+Value* JSON::deserialize(const std::string& string)
+{
+	JSON::position = 0;
+	_deserealize(string);
+}
+
+Value* _deserealize(const std::string& string) {
+	std::string nfirstchar = "-0123456789.";
+	skipWhiteSpace(string);
+	if (JSON::position == string.length()) return new NullValue();
+	char c = string[JSON::position];
+	if (c == '{') return parseObject(string);
+	else if (c == '[') return parseArray(string);
+	else if (c == '\"') return parseString(string, '\"');
+	else if (c == '\'') return parseString(string, '\'');
+	else if (nfirstchar.find(c) != std::string::npos) return parseNumber(string);
+	else if (c == 't') return parseLiteral(string, "true");
+	else if (c == 'f') return parseLiteral(string, "false");
+	else if (c == 'n') return parseLiteral(string, "null");
+	throw std::exception("invalid json");
+}
+
+Value* parseObject(const std::string& string){
+	JSON::position++;
+	ObjectValue* ov = new ObjectValue();
+	char c;
+	while (true) {
+		Value* key = _deserealize(string);
+		skipWhiteSpace(string);
+		c = string[JSON::position];
+		if (c != ':') throw std::exception("Expecting :");
+		JSON::position++;
+		skipWhiteSpace(string);
+		Value* value = _deserealize(string);
+		skipWhiteSpace(string);
+		ov->append(KeyValuePair(dynamic_cast<StringValue*>(key)->get(), value));
+		c = string[JSON::position];
+		if (c == ',') {
+			JSON::position++;
+			skipWhiteSpace(string);
+		}
+		else break;
+	}
+	if (c != '}') {
+		// Response.Write(str.substring(JSON::position));
+		throw std::exception("Expecting }");
+	}
+	JSON::position++;
+	return ov;
+}
+
+Value* parseArray(const std::string& string) {
+	JSON::position++;
+	skipWhiteSpace(string);
+	if (string[JSON::position] == ']') {
+		JSON::position++;
+		return new ArrayValue();
+	}
+	ArrayValue* a = new ArrayValue();
+	char c;
+	while (true) {
+		Value* value = _deserealize(string);
+		a->append(value);
+		skipWhiteSpace(string);
+		c = string[JSON::position];
+		if (c == ',') {
+			JSON::position++;
+			skipWhiteSpace(string);
+		}
+		else break;
+	}
+	if (c != ']') {
+		throw "Expecting ]";
+	}
+	JSON::position++;
+	return a;
+}
+
+Value* parseString(const std::string& string, char quote) {
+	int startIndex = ++JSON::position;
+	char c;
+	std::string str = "";
+	while ((c = string[JSON::position]) != quote) {
+		if (c == '\\') {
+			JSON::position++;
+			c = string[JSON::position];
+			if (c == 'r') str += '\r';
+			else if (c == 'n') str += '\n';
+			else if (c == 't') str += '\t';
+			else if (c == 'f') str += '\f';
+			// Note escaped unicode not handled
+			else str+=c;
+		}
+		else str+=c;
+		JSON::position++;
+	}
+	JSON::position++;
+	return new StringValue(str);
+}
+
+Value* parseNumber(const std::string& string) {
+	int startIndex = JSON::position;
+	int len = 0;
+	char c;
+	std::string nfirstchar = "-0123456789.";
+	while (nfirstchar.find(string[JSON::position]) != std::string::npos) {
+		JSON::position++;
+		len++;
+	}
+	std::string substring = string.substr(startIndex, len);
+	return new NumberValue(std::stod(substring));
+}
+
+Value* parseLiteral(const std::string& string, std::string literal) {
+	if (literal.length() > string.length() - JSON::position) {
+		throw "Expecting " + literal;
+	}
+	for (size_t i = 0; i < literal.length(); i++) {
+		if (literal[i] != string[i++]) {
+			throw "Expecting " + literal;
+		}
+	}
+	if (literal == "true") { 
+		return new BoolValue(true);
+	}
+	else if(literal == "false"){
+		return new BoolValue(false);
+	}
+	else {
+		return new NullValue();
+	}
+}
+
+void skipWhiteSpace(const std::string& string) {
+	while (JSON::position < string.length() && isWhiteSpace(string[JSON::position])) JSON::position++;
+}
+
+bool isWhiteSpace(char c) {
+	return c == ' ' || c == '\r' || c == '\n' || c == '\t';
+}
+
+// - provede serializaci do JSON řetězce
+std::string JSON::serialize(const Value* value)
+{
+	return value->serialize();
+}
---
+++ semestralka/JsonLibrary/JsonLibrary/api.h
@@ -0,0 +1,240 @@
+﻿#include <string>
+#include <exception>
+// - šablona s parametrem datového typu uložených hodnot
+// - není povoleno užití STL kontejnerů ani jiných knihoven pro ukládání dat
+// - realizace musí využívat dynamicky alokované pole, spojový seznam nebo jinou vhodnou Vámi implementovanou ADS 
+
+template<typename T>
+class DynamicArray {
+	struct Node {
+		Node* next;
+		T data;
+		Node(const T& data) {
+			this->data = data;
+			next = nullptr;
+		}
+	};
+public:
+	DynamicArray();
+	~DynamicArray();
+
+	// - přidá element na konec pole
+	void append(const T& element);
+
+	// - výjimky při neplatném nebo nekorektním indexu
+	const T& getElementAt(int index) const;
+
+	// - vrací velikost (počet prvků) v poli
+	int getSize() const;
+private:
+	Node* first;
+	Node* actual;
+	Node* last;
+	int size;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// - definuje pár klíč (řetězec) a hodnota (JSON hodnota) pro reprezentaci hodnot JSON objektu
+class KeyValuePair {
+public:
+	KeyValuePair(std::string key, Value* value);
+
+	// - vrátí klíč
+	std::string getKey() const;
+	// - vrátí hodnotu
+	Value* getValue() const;
+private:
+	std::string key;
+	Value* value;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// JSON hodnota - reprezentuje abstraktního předka pro základní datové typy v JSON (string, number, object, array, bool, null)
+class Value
+{
+public:
+	// serializuje hodnotu do podoby JSON reprezentace
+	virtual std::string serialize() const = 0;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// - reprezentuje hodnotu typu JSON null
+class NullValue :
+	public Value
+{
+public:
+	std::string serialize() const override;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// - reprezentuje hodnotu typu JSON bool
+class BoolValue :
+	public Value
+{
+public:
+	BoolValue(bool value);
+
+	// - vrací bool hodnotu
+	bool get() const;
+	std::string serialize() const override;
+private:
+	bool value;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// - reprezentuje hodnotu typu JSON číslo
+class NumberValue :
+	public Value
+{
+public:
+	NumberValue(double value);
+
+	// - vrací číselnou hodnotu 
+	std::string serialize() const override;
+	double get() const;
+private:
+	double value;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// - reprezentuje hodnotu typu JSON řetězec (string)
+class StringValue :
+	public Value
+{
+public:
+	StringValue(std::string value);
+
+	// - vrací řetězcovou hodnotu
+	std::string get() const;
+	void append(char c);
+	std::string serialize() const override;
+private:
+	std::string value;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// - reprezentuje hodnotu typu JSON pole
+class ArrayValue :
+	public Value
+{
+public:
+	ArrayValue();
+	~ArrayValue();
+
+	// - přidá element na konec pole
+	void append(Value* element);
+	std::string serialize() const override;
+private:
+	// - atribut DynamicArray<Value*> pro uchování jednotlivých elementů v poli
+	DynamicArray<Value*> array;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// - reprezentuje hodnotu typu JSON objekt
+class ObjectValue :
+	public Value
+{
+public:
+
+	ObjectValue();
+	~ObjectValue();
+
+	// - přidá klíč-element do objektu
+	void append(const KeyValuePair& pair);
+	std::string serialize() const override;
+private:
+	// - atribut DynamicArray<KeyValuePair> pro uchování jednotlivých hodnot a klíčů v objektu
+	DynamicArray<KeyValuePair> array;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// - třída pro práci s JSON
+class JSON
+{
+public:
+	// - provede deserializaci řetězce na vytvořené objekty
+	// - přečtu znak a rozhodnu se (uvedený postup předpokládá čtení bez přeskakování bílých znaků)
+	// -- '{' - začínám číst objekt
+	// -------- čtu znaky, pak musí být dvojtečka, potom volám rekurzivně deserialize();  následuje čárka nebo '}', podle situace se čtení opakuje
+	// -- '[' - začínám číst pole
+	// -------- volám rekurzivně deserialize(); následuje čárka nebo ']', podle situace se čtení opakuje
+	// -- '"' - začínám číst řetězec - pozor na escapované uvozovky
+	// -- [-0123456789] - začínám číst číslo - načtu všechny číslice (pozor na možnou desetinnou  tečku)
+	// -- 'n' - 'null' – načtu zbylé znaky a kontroluji, že je to opravdu ‚null‘ a ne něco jiného
+	// -- 't' - 'true' - dtto
+	// -- 'f' - 'false' - dtto
+	// -- cokoliv jiného - vyvolávám výjimku
+	// - není přípustné vracet nullptr
+	// - deserializace musí být rozumně implementována - není přípustné zde napsat jednu extrémně  dlouhou metodu
+	static Value* deserialize(const std::string& string);
+
+	// - provede serializaci do JSON řetězce
+	static std::string serialize(const Value* value);
+	static size_t position; // pointer to actual position in array
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+template<typename T>
+inline DynamicArray<T>::DynamicArray()
+{
+	first = nullptr;
+	actual = nullptr;
+	size = 0;
+}
+
+template<typename T>
+inline DynamicArray<T>::~DynamicArray()
+{
+	while (first != nullptr) {
+		Node* tmp = first->next;
+		delete first;
+		first = tmp;
+	}
+}
+
+template<typename T>
+inline void DynamicArray<T>::append(const T& element)
+{
+	if (first == nullptr) {
+		first = new Node(element);
+	}
+	else {
+		actual = first;
+		while (actual.next != nullptr) {
+			actual = actual.next;
+		}
+		actual.next = new Node(element);
+	}
+	size++;
+}
+
+template<typename T>
+inline const T& DynamicArray<T>::getElementAt(int index) const
+{
+	if (index > size - 1) {
+		throw std::exception("wrong index");
+	}
+	actual = first;
+	for (size_t i = 0; i < index; i++)
+	{
+		actual = actual.next;
+	}
+	return &actual.data;
+}
+
+template<typename T>
+inline int DynamicArray<T>::getSize() const
+{
+	return size;
+}
